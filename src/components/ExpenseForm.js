689840d2d08d2bbc9bc3992754362a320ac5a797
MOHAMAD ABBAS <mabbas85@gmail.com>
2018-04-14 12:38:36 +0200

    Add Expensify styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -10,7 +10,7 @@ export default class ExpenseForm extends React.Component {
         this.state = {
             description: props.expense ? props.expense.description : '',
             note: props.expense ? props.expense.note : '',
-            amount: props.expense ? (props.expense.amount/100).toString() : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
             createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
             calendarFocused: false,
             error: ''
@@ -64,39 +64,42 @@ export default class ExpenseForm extends React.Component {
 
     render() {
         return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmit}>
-                    <input
-                        type="text"
-                        placeholder="Description"
-                        autoFocus
-                        value={this.state.description}
-                        onChange={this.onDescriptionChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Amount"
-                        value={this.state.amount}
-                        onChange={this.onAmountChange}
-                    />
-                    <SingleDatePicker
-                        date={this.state.createdAt}
-                        onDateChange={this.onDateChange}
-                        focused={this.state.calendarFocused}
-                        onFocusChange={this.onFocusChange}
-                        numberOfMonths={1}
-                        isOutsideRange={() => false}
-                    />
-                    <textarea
-                        placeholder="Add a note for your expense (optional)"
-                        value={this.state.note}
-                        onChange={this.onNoteChange}
-                    >
+            <form className="form" onSubmit={this.onSubmit}>
+                {this.state.error && <p className="form__error">{this.state.error}</p>}
+                <input
+                    className="text-input"
+                    type="text"
+                    placeholder="Description"
+                    autoFocus
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                />
+                <input
+                    className="text-input"
+                    type="text"
+                    placeholder="Amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                />
+                <SingleDatePicker
+                    date={this.state.createdAt}
+                    onDateChange={this.onDateChange}
+                    focused={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
+                <textarea
+                    className="textarea"
+                    placeholder="Add a note for your expense (optional)"
+                    value={this.state.note}
+                    onChange={this.onNoteChange}
+                >
                     </textarea>
-                    <button>Add Expense</button>
-                </form>
-            </div>
+                <div>
+                    <button className="button">Save Expense</button>
+                </div>
+            </form>
         )
     }
 }
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,14 +3,20 @@ import {connect} from "react-redux";
 import selectExpensesTotal from '../selectors/expenses-total'
 import selectExpenses from "../selectors/expenses";
 import numeral from 'numeral';
+import {Link} from "react-router-dom";
 
 export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
 
     return (
-        <div>
-            <h1>
-                Viewing {expensesCount} expense{expensesCount === 1 ? '' : 's'} totalling {numeral(expensesTotal / 100).format('$0,0.00')}
-            </h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">
+                    Viewing <span>{expensesCount}</span> expense{expensesCount === 1 ? '' : 's'} totalling <span>{numeral(expensesTotal / 100).format('$0,0.00')}</span>
+                    <div className="page-header__actions">
+                        <Link className="button" to="/create" activeClassName="is-active">Add Expense</Link>
+                    </div>
+                </h1>
+            </div>
         </div>
     )
 };
